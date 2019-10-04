72939d7a7f102f3188e3ebb75fdb7ffa5dbd512b
remorses <beats.by.morse@gmail.com>
2019-10-04 17:00:50 +0200

    added support stuff

---
+++ src/support.ts
@@ -0,0 +1,18 @@
+import decodeJwt from 'decode-jwt'
+import { LOCAL_STORAGE_TOKEN_KEY } from './constants'
+
+export const getUserId = () => {
+    const token = localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY)
+    if (token) {
+        const { user_id } = decodeJwt(token)
+        return user_id
+    }
+}
+
+export const clearJwt = () => {
+    localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY)
+}
+
+export const setJwt = (jwt: string) => {
+    localStorage.setItem(LOCAL_STORAGE_TOKEN_KEY, jwt)
+}
